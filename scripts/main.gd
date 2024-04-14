f6f54068a8d40d6ef1ad4e08562d57fc9460dcaa
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-15 04:49:09 +1200

    music fix

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var bgm_player = $BGMPlayer
 
+var music_array = ["res://resources/audio/Music_test_1.wav", "res://resources/audio/Music_test_2.wav", "res://resources/audio/Music_test_3.wav", "res://resources/audio/Music_test_4.wav", "res://resources/audio/Music_test_5.wav"]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Singleton.game_won.connect(game_has_been_won)
@@ -9,3 +11,7 @@ func _ready():
 
 func game_has_been_won() -> void:
 	bgm_player.stop()
+
+
+func _on_bgm_player_finished():
+	bgm_player.play()
