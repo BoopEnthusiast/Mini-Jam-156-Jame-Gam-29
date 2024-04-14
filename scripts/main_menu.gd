9ea1c8dc2820739c179deb6d7a91b5431baa2c30
Boop <onpotis@gmail.com>
2024-04-14 21:04:52 +1200

    faster loading

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,23 +1,13 @@
 extends Control
 
-@export var game:PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+const MAIN = preload("res://scenes/main.tscn")
 
 
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
 			Singleton.timer.start(300)
-			get_tree().change_scene_to_packed(game)
+			get_tree().change_scene_to_packed(MAIN)
 
 
-func _on_audio_stream_player_finished():
-	$AudioStreamPlayer.play()
