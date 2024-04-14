861d7e6f7439d99e1d7230155bcf095ac6ee0e8f
Boop <onpotis@gmail.com>
2024-04-14 18:11:31 +1200

    Moving theme + NPC stuff

---
+++ scripts/npc.gd
@@ -0,0 +1,11 @@
+class_name NPC extends StaticBody2D
+
+@export var dialogue: String = "Text goes here..."
+
+
+func _on_talk_area_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_talk_area_body_exited(body):
+	pass # Replace with function body.
